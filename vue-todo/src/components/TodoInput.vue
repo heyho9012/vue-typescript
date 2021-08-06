c11c89bd9e0d509cdc9ac40bdc2492aa4c0e169a
heyho9012 <heyho9012@gmail.com>
2021-08-06 16:15:03 +0900

    Add vue-todo with typescript

---
+++ vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="input_box">
+    <label for="todo-input">오늘 할 일 : </label>
+    <input type="text" id="todo-input" :value="item" @input="handleInput" />
+    <button @click="addTodo" type="button">add</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  props: {
+    item: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    handleInput(event: InputEvent) {
+      // if (!event.target) {
+      //   return;
+      // }
+      const eventTarget = event.target as HTMLInputElement;
+      this.$emit('input', eventTarget.value);
+    },
+    addTodo() {
+      this.$emit('add');
+    },
+  },
+});
+</script>
+
+<style scoped></style>
